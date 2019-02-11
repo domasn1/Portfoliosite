90f77a5cc9fc563e42d4d1c40ff7da679cb8cfd5
Sofia Gattuso <sofia@Sofias-MacBook-Pro.local>
2019-02-11 15:52:12 +0000

    update

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/bootstrap-4.1.0-dist'));
 
 app.get('/', function (req, res) {
- res.sendfile('./index.html');
+ res.sendfile('./bootstrap-4.1.0-dist/index.html');
 });
 
 app.listen(port, function () {
